e9071c21194bf6d6da0e401432562bc667614264
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-06-21 21:45:59 +0200

    Say Hello function.

--- src/main/java/pps/MainView.java
+++ src/main/java/pps/MainView.java
@@ -1,7 +1,15 @@
 package pps;
 
 public class MainView {
+    /**
+     * JavaDoc comment for Say Hello function.
+     */
+    void sayHello() {
+        System.out.println("Hello World!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Start new application.");
+        MainView view = new MainView();
+        view.sayHello();
     }
 }
