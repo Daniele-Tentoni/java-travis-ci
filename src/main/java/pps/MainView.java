9f8e933e0f45faa97c0e674ec13eb8c22d0b722c
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-06-21 23:36:06 +0200

    Update spring boot

--- src/main/java/pps/MainView.java
+++ src/main/java/pps/MainView.java
@@ -1,15 +1,28 @@
 package pps;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
+
 public class MainView {
+    @GetMapping("/info")
+    public String info() {
+        return "I'm a Spring Boot base microservice.";
+    }
+
     /**
      * JavaDoc comment for Say Hello function.
      */
-    void sayHello() {
+    @GetMapping("/hello")
+    public String sayHello() {
         System.out.println("Hello World!");
+        return "Hello, my world!";
     }
 
     public static void main(String[] args) {
-        MainView view = new MainView();
-        view.sayHello();
+        final SpringApplication app = new SpringApplication(MainView.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.run(args);
     }
 }
