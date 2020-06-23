958bc61895a47e7918e5fabfaab558877387d32f
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-06-23 14:56:42 +0200

    New index page

--- src/main/java/pps/controllers/PuzzleController.java
+++ src/main/java/pps/controllers/PuzzleController.java
@@ -27,4 +27,18 @@ public class PuzzleController {
     public Set<Box> getMapping() {
         return PuzzleStore.instance().getBoxes();
     }
+
+    @GetMapping("/state")
+    @ResponseBody
+    public String getGameState() {
+        if(PuzzleStore.instance().isFinished()) {
+            return "Finished";
+        }
+
+        if(PuzzleStore.instance().isGenerated()) {
+            return "Generated";
+        }
+
+        return "Nothing";
+    }
 }
