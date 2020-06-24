8202722dcf7cd1c13cad0f60a0d5af3b378e1a3f
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-06-24 15:29:25 +0200

    Original position promotion

--- src/main/java/pps/model/Box.java
+++ src/main/java/pps/model/Box.java
@@ -3,20 +3,14 @@ package pps.model;
 import java.util.Optional;
 
 public class Box implements Comparable<Box> {
-    int id;
     Optional<String> taken;
     private final int originalPosition;
     private int currentPosition;
 
-    public Box(final int id, final int originalPosition, final int currentPosition) {
+    public Box(final int originalPosition, final int currentPosition) {
         taken = Optional.empty();
-        this.id = id;
         this.originalPosition = originalPosition;
         this.currentPosition = currentPosition;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public void take(String id) {
@@ -27,12 +21,16 @@ public class Box implements Comparable<Box> {
         return taken.isPresent();
     }
 
-    public boolean isTaken(String id) {
-        return taken.isPresent() && taken.get().equals(id);
+    public boolean isTaken(String name) {
+        return taken.isPresent() && taken.get().equals(name);
     }
 
     public int getCurrentPosition() {
         return currentPosition;
+    }
+
+    public int getOriginalPosition() {
+        return originalPosition;
     }
 
     public boolean isInRightPlace() {
--- src/main/java/pps/stores/PuzzleStore.java
+++ src/main/java/pps/stores/PuzzleStore.java
@@ -30,18 +30,25 @@ public class PuzzleStore {
     }
 
     public void take(String player, int id) {
-        tiles.stream().filter(f -> f.getId() == id).findFirst().ifPresent(p -> {
+        tiles.stream().filter(f -> f.getOriginalPosition() == id).findFirst().ifPresent(p -> {
             if (!p.isTaken())
                 p.take(player);
         });
     }
 
     public void move(String player, int first, int second) {
-        tiles.stream().filter(f -> f.getId() == first).findFirst().ifPresent(p -> tiles.stream().filter(g -> g.getId() == second).findFirst().ifPresent(q ->{
-            int pos = p.getCurrentPosition();
-            p.setCurrentPosition(player, q.getCurrentPosition());
-            q.setCurrentPosition(player, pos);
-        }));
+        tiles.stream()
+                .filter(f -> f.getOriginalPosition() == first)
+                .findFirst()
+                .ifPresent(p ->
+                        tiles.stream()
+                                .filter(g -> g.getOriginalPosition() == second)
+                                .findFirst()
+                                .ifPresent(q -> {
+                                    int pos = p.getCurrentPosition();
+                                    p.setCurrentPosition(player, q.getCurrentPosition());
+                                    q.setCurrentPosition(player, pos);
+                                }));
         checkSolution();
     }
 
@@ -65,19 +72,19 @@ public class PuzzleStore {
         int position = 0;
 
         final List<Integer> randomPositions = new ArrayList<>();
-        IntStream.range(0, rows*columns).forEach(randomPositions::add);
+        IntStream.range(0, rows * columns).forEach(randomPositions::add);
         Collections.shuffle(randomPositions);
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                tiles.add(new Box( i * columns + j, position, randomPositions.get(position)));
+                tiles.add(new Box(position, randomPositions.get(position)));
                 position++;
             }
         }
     }
 
     private void checkSolution() {
-        if(tiles.stream().allMatch(Box::isInRightPlace)) {
+        if (tiles.stream().allMatch(Box::isInRightPlace)) {
             this.finished = true;
         }
     }
